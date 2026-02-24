60acaf8ce3ac0ff4329b1279432c61c780ee77c9
Nicola Puoti <np84digitalproduct@gmail.com>
2026-02-24 17:58:53 +0100

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,1 +1,10 @@
+output "vpc_id" { value = aws_vpc.p2.id }
 
+output "vpc_endpoints" {
+  value = {
+    s3_gateway  = aws_vpc_endpoint.s3_gateway.id
+    ssm         = aws_vpc_endpoint.ssm.id
+    ssmmessages = aws_vpc_endpoint.ssmmessages.id
+    ec2messages = aws_vpc_endpoint.ec2messages.id
+  }
+}
