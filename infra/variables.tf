29ca3236e08db11a66e428489cec25d3027b8f07
Nicola Puoti <np84digitalproduct@gmail.com>
2026-02-24 17:54:35 +0100

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,1 +1,5 @@
-
+variable "aws_region" { type = string, default = "eu-central-1" }
+variable "vpc_name"   { type = string, default = "p2-vpc" }
+variable "vpc_cidr"   { type = string, default = "10.0.0.0/16" }
+variable "az_a"       { type = string, default = "eu-central-1a" }
+variable "az_b"       { type = string, default = "eu-central-1b" }
